f247ac2b3ffbfafb2c663109b5cbb46071f746e9
gabrielWilfried <gabrielwilfried0808@gmail.com>
2024-07-11 20:06:00 +0200

    Change DEbUG to true

--- sym_crypto/settings.py
+++ sym_crypto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vux^6r@^9e!3!cuk6zbk5xlr=a6cj_eh_tn*uyv3=yu^3#1$$e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['crypto-aes-des.onrender.com']
 
